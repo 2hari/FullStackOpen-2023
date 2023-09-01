05c50b550741c4543be0f2348ee88eb413a6f07a
2hari <two1821125run@protonmail.com>
2023-09-01 11:15:23 +0530

    exercise 9.14-9.15 complete

---
+++ part9/course-app/src/components/Content.tsx
@@ -0,0 +1,12 @@
+import Part from './Part'
+import { CoursePart } from '../types'
+
+const Content = ({parts}:{parts:CoursePart[]}) => {
+  return (
+    <div>
+      {parts.map( part => <Part key={part.name} part={part}/>)}
+    </div>
+  )
+}
+
+export default Content
---
+++ part9/course-app/src/components/Part.tsx
@@ -0,0 +1,41 @@
+import { CoursePart } from "../types"
+import { assertNever } from "../utils";
+
+const Part = ({ part }: { part: CoursePart }) => {
+    switch(part.kind) {
+      case "basic":
+        return(
+          <div>
+            <h3>{part.name} {part.exerciseCount}</h3>
+            <p><i>{part.description}</i></p>
+          </div>
+        );
+      case "group":
+        return(
+          <div>
+            <h3>{part.name} {part.exerciseCount}</h3>
+            <p>project exercises {part.groupProjectCount}</p>
+          </div>
+        );
+      case "background":
+        return (
+          <div>
+            <h3>{part.name} {part.exerciseCount}</h3>
+            <p><i>{part.description}</i></p>
+            <p>submit to {part.backgroundMaterial}</p>
+          </div>
+        );
+      case "special":
+        return (
+          <div>
+            <h3>{part.name} {part.exerciseCount}</h3>
+            <p><i>{part.description}</i></p>
+            <p>required skills: {part.requirements.join(", ")}</p>
+          </div>
+        );
+      default:
+        return assertNever(part);
+    }  
+}
+
+export default Part
